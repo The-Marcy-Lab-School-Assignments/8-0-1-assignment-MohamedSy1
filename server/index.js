a9bbe7947d62c48afefa42973d47f08ead469430
Mohamed <mohamed.sy@compscihigh.org>
2024-04-11 17:37:46 -0400

    revised short-responce and fixed server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
-const express = require("express")
-const path = require('path')
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const pathToDistFolder = path.join(__dirname, '..', 'path', 'front-end', 'dist')
+const pathToDistFolder = path.join(__dirname, "..", "front-end", "dist");
 
-const serveStatic = express.static(pathToDistFolder)
+const logRoutes = (req, res, next) => {
+    const time = (new Date()).toLocaleString()
+    console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+    next();
+}
 
-app.use(serveStatic)
-const port = 8080
-app.listen(port, () => { console.log(`this is the port: ${port}`) })
+app.use(logRoutes)
+app.use(express.static(pathToDistFolder));
+
+
+const port = 8080;
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
